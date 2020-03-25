f1ffb9ea4b1df0a829d94de1c62f735b6ab36b62
Aty <aty99@yahoo.com>
2020-03-25 18:20:17 +0200

    Initial Commit

--- NiceSwipeing/ViewController.swift
+++ NiceSwipeing/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIScrollViewDelegate {
+    
+    
+    var image:[String] = ["diamond","Pic1","Pic2"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
+    @IBOutlet var pageControl: UIScrollView!
+    @IBOutlet var scrollView: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+          pageControl.delegate = self
+        
+        
+        scrollView.numberOfPages = image.count
+        
+        for index in 0..<image.count {
+            frame.origin.x = pageControl.frame.size.width * CGFloat(index)
+            frame.size = pageControl.frame.size
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: image[index])
+            self.pageControl.addSubview(imgView)
+        }
+        
+        pageControl.contentSize = CGSize(width: (pageControl.frame.size.width * CGFloat(image.count)) ,height: pageControl.frame.size.height)
+    
     }
 
-
+    //ScrollView Method
+    //================
+   
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let pageNumber = pageControl.contentOffset.x / pageControl.frame.size.width
+        self.scrollView.currentPage = Int (pageNumber)
+        
+    }
+    
 }
 
